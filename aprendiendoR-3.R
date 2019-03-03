d34b44d1285406b6478429816dcbb432bea24357
hmendozar17 <hmendozar@gmail.com>
2019-03-03 11:19:55 -0800

    Add files via upload

---
+++ aprendiendoR-3.R
@@ -0,0 +1,8 @@
+
+
+#Importar datos
+#Cambiando de directorio 
+software<- read.csv2("software.txt", sep="\",header=TRUE)
+#sIN cambiar de directorio 
+software<- read.csv2("software.txt", sep="\",header=TRUE)
+C:\Users\glorobame\Documents\Gloria\AprendiendoR
